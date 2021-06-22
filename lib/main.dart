94fc791cc4620d7ed9d47dcc990b4b519e6f5101
Hassaan Ashraf <engr.hassaanashraf@gmail.com>
2021-06-22 12:06:26 +0500

    Signup Form Data Passing from One screen to other

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'signup_form.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Data Sharing",
+    home: ExtrctScaffold(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
